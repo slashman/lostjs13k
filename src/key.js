372b5c338c15b1df04240304df41df47d5cb277f
slash <slash@slashware.net>
2017-08-27 18:09:56 -0500

    Fix to allow several keypress events

--- src/key.js
+++ src/key.js
@@ -1,14 +1,23 @@
 const pressed = {};
+const typedCallbacks = {};
+
+function keyPress(e){
+	if (typedCallbacks[e.keyCode]){
+		typedCallbacks[e.keyCode]();
+	}
+}
 
 module.exports = {
 	init: function(){
 		window.onkeydown = function(e) {pressed[e.keyCode] = true};
 		window.onkeyup = function(e) {pressed[e.keyCode] = false};
+		window.addEventListener("keypress", keyPress);
 	},
 	isDown: function(keyCode){
 		return pressed[keyCode];
 	},
 	typed: function(keyCode, callback){
-		window.addEventListener("keypress", callback);
+		typedCallbacks[keyCode] = callback;
+		
 	}
 }
